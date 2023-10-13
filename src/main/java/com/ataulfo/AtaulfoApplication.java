9aea87d9399aac01c5fbae41f2fac6ede607b365
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-13 11:23:51 +0530

    initial commit

---
+++ src/main/java/com/ataulfo/AtaulfoApplication.java
@@ -0,0 +1,13 @@
+package com.ataulfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaulfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaulfoApplication.class, args);
+	}
+
+}
