bef85ab60e66c78119ab50ce654d0eb697bb8bf1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-13 11:31:41 +0530

    'Ataúlfo' mango is a mango cultivar from Mexico

--- src/main/java/com/ataulfo/AtaulfoApplication.java
+++ src/main/java/com/ataulfo/AtaulfoApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtaulfoApplication {
+public class AtaulfoApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(AtaulfoApplication.class, args);
+		System.out.println("\n  'Ataúlfo' mango is a mango cultivar from Mexico");
 	}
 
 }
