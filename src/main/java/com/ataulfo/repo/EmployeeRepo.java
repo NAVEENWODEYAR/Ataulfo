8251ae6e2c6c7344c955a07b84d42708db62f168
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-09 21:36:59 +0530

    @Query(value = "",nativeQuery = true)

--- src/main/java/com/ataulfo/repo/EmployeeRepo.java
+++ src/main/java/com/ataulfo/repo/EmployeeRepo.java
@@ -1,10 +1,12 @@
 package com.ataulfo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ataulfo.modal.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> 
 {
-
+	@Query(value = "",nativeQuery = true)
+	public void getEmp();
 }
