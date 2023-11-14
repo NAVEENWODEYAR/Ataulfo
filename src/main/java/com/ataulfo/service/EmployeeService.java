b18971148e2958eaf09d2301b6b7c7279eb58fd9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-14 21:52:19 +0530

    public Employee updateEmployee(@PathVariable int empId, @RequestBody Employee emp)

--- src/main/java/com/ataulfo/service/EmployeeService.java
+++ src/main/java/com/ataulfo/service/EmployeeService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ataulfo.dto.EmployeeDTO;
 import com.ataulfo.modal.Employee;
@@ -34,5 +36,16 @@ public class EmployeeService
 	public Employee getEmployeeById(int empId)
 	{
 		return employeeRepo.findById(empId).get();
+	}
+	
+	// 4. UPDATE TABLENAME SET email = '' 	WHERE id = '';  
+	public Employee updateEmployee(@PathVariable int empId, @RequestBody Employee emp)
+	{
+		Employee e = employeeRepo.findById(empId).get();
+					e.getEmpMail(emp.setEmpMail(""));
+					e.getEmpName(emp.setEmpName(""));
+					e.getEmpPhone(emp.setEmpPhone());
+					e.getEmpSalary(emp.setEmpSalary());
+					;
 	}
 }
