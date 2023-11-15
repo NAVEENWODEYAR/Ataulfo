ca6059c9133a328942490a268654a6a0f2fe5da3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-15 13:39:48 +0530

    errors

--- src/test/java/com/ataulfo/AtaulfoApplicationTests.java
+++ src/test/java/com/ataulfo/AtaulfoApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AtaulfoApplicationTests {
+class AtaulfoApplicationTests 
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		
 	}
 
 }
